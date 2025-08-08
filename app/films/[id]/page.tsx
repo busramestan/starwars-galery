51d2ee7ae86fbf8e773a11274c91f0323535c9a4
Busra Mestan <bmestan50@gmail.com>
2025-08-08 03:47:36 +0300

    update: text color

--- app/films/[id]/page.tsx
+++ app/films/[id]/page.tsx
@@ -8,9 +8,9 @@ export default async function FilmDetail({params} :{params: {id: string}}){
         notFound()
     }
     return(
-        <div className="min-h-screen bg-indigo-500 text-white p-8">
+        <div className="min-h-screen bg-gray-800 text-white p-8">
             <h1 className="text-4xl font-bold mb-4">Film: {film.title}</h1>
-            <h3 className="text-xl font-semibold mb-4 text-blue-950">Director: {film.director}</h3>
+            <h3 className="text-xl font-semibold mb-4 text-gray-400">Director: {film.director}</h3>
             <p className="text-lg">{film.opening_crawl}</p>
         </div>
     )
